6313d28191b445c33a9e4457e0f90f804cd80458
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-06-10 19:07:14 -0400

    Create Xcode project

---
+++ stockspotter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  stockspotter
+//
+//  Created by Mohammed Al-Dahleh on 2018-06-10.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ stockspotter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  stockspotter
+//
+//  Created by Mohammed Al-Dahleh on 2018-06-10.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
