976e2239b64ed93f298abda911933e4146ea5089
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-06-11 01:51:53 -0400

    Abstract networking code

--- stockspotter/StockCollectionView/StockCollectionDataSource/StockCollectionDataSource.m
+++ stockspotter/StockCollectionView/StockCollectionDataSource/StockCollectionDataSource.m
@@ -7,6 +7,7 @@
 //
 
 #import "StockCollectionDataSource.h"
+#import "StockData.h"
 
 @interface StockCollectionDataSource ()
 @property (nonatomic, strong) NSMutableArray *stockData;
@@ -16,13 +17,27 @@
 
 static NSString * const reuseIdentifier = @"StockCell";
 
--(id)init {
-    [self updateData];
-    return self;
-}
-
 - (void)updateData {
     self.stockData = [NSMutableArray array];
+    [self refreshCells];
+}
+
+- (void)refreshCells {
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURL *url = [NSURL URLWithString:@"https://api.iextrading.com/1.0/stock/market/list/mostactive"];
+    
+    NSURLSessionDownloadTask *task = [session downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSData *data = [[NSData alloc] initWithContentsOfURL:location];
+        NSArray *stockData = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+        
+        for (NSDictionary *dict in stockData) {
+            NSLog(@"HIT");
+            StockData *dataObject = [StockData stockWithDictionary:dict];
+            [self.stockData addObject:dataObject];
+        }
+    }];
+    
+    [task resume];
 }
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
@@ -37,7 +52,8 @@ static NSString * const reuseIdentifier = @"StockCell";
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
     
-    // Configure cell
+    StockData *stockAtCell = [self.stockData objectAtIndex:indexPath.row];
+    // TODO: Configure cell
     
     return cell;
 }
--- stockspotter/StockCollectionView/StockCollectionViewController.m
+++ stockspotter/StockCollectionView/StockCollectionViewController.m
@@ -11,7 +11,6 @@
 #import "StockData.h"
 
 @interface StockCollectionViewController ()
-@property (strong, nonatomic) NSMutableArray *stockCollection;
 @property (nonatomic, strong) StockCollectionDataSource *dataSource;
 @end
 
@@ -29,25 +28,6 @@
 - (void)refreshData {
     [self.dataSource updateData];
     [self.collectionView reloadData];
-}
-
-- (void)refreshCells {
-    NSURLSession *session = [NSURLSession sharedSession];
-    NSURL *url = [NSURL URLWithString:@"https://api.iextrading.com/1.0/stock/market/list/mostactive"];
-    
-    self.stockCollection = [NSMutableArray array];
-    
-    NSURLSessionDownloadTask *task = [session downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSData *data = [[NSData alloc] initWithContentsOfURL:location];
-        NSArray *stockData = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
-        
-        for (NSDictionary *dict in stockData) {
-            StockData *dataObject = [StockData stockWithDictionary:dict];
-            [self.stockCollection addObject:dataObject];
-        }
-    }];
-    
-    [task resume];
 }
 
 @end
