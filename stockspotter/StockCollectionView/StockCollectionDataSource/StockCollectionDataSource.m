62c8d795df1fdf07b5457d6ce1cf741f7399d2e7
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-06-11 22:11:27 -0400

    Update displayed stocks

--- stockspotter/StockCollectionView/StockCollectionDataSource/StockCollectionDataSource.m
+++ stockspotter/StockCollectionView/StockCollectionDataSource/StockCollectionDataSource.m
@@ -25,7 +25,7 @@ static NSString * const reuseIdentifier = @"StockCell";
 
 - (void)refreshCells:(UICollectionView*)collectionView {
     NSURLSession *session = [NSURLSession sharedSession];
-    NSURL *url = [NSURL URLWithString:@"https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,amzn,msft,fb,brk.b,jpm,xom,jnj,v,bac,wfc,intc,wmt,cvx,unh,hd,ba,pfe,ma,t,csco,vz,orcl,pg,ko,c,mrk,nvda,nflx,dis,mcd,ibm,adbe,nke,ge,hon,txn,unp,bkng,ups,pypl,crm,cat,lmt,cost,gs,axp,low,sbux,twx,khc,fdx,cvs,chtr,gm,rtn,gd,fox,aaba,tsla,mar,ttwo,z,has,dbx,trip,colm,jblu,mat,wen,noc,bk,f,tgt,hpq,race,twtr,spot,luv,tri,cp,sq,k,bby,cbs,hsy,snap,efx,shop,qsr,tru,ihg,cmg,gddy,m,dpz,wu,grub,h&types=quote"];
+    NSURL *url = [NSURL URLWithString:@"https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fun,msft,fb,brk.b,jpm,xom,jnj,v,bac,wfc,intc,wmt,cvx,unh,hd,ba,pfe,ma,t,csco,vz,orcl,pg,ko,c,mrk,nvda,nflx,dis,mcd,ibm,adbe,nke,ge,hon,txn,unp,six,ups,pypl,crm,cat,lmt,cost,gs,axp,low,sbux,twx,khc,fdx,cvs,chtr,gm,rtn,gd,fox,aaba,tsla,mar,ttwo,z,has,dbx,trip,colm,jblu,mat,wen,noc,bk,f,tgt,hpq,race,twtr,spot,luv,tri,cp,sq,k,bby,cbs,hsy,snap,mtn,shop,qsr,tru,ihg,cmg,gddy,m,dpz,wu,grub,h&types=quote"];
     
     NSURLSessionDownloadTask *task = [session downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         NSData *data = [[NSData alloc] initWithContentsOfURL:location];
