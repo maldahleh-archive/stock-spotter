7f4b6c6b6dbcb9f4e65e45478a342ff972147a8f
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-06-12 22:07:39 -0400

    Add header view to stock display

---
+++ stockspotter/StockCollectionView/StockHeader/StockHeader.h
@@ -0,0 +1,13 @@
+//
+//  StockHeader.h
+//  StockSpotter
+//
+//  Created by Mohammed Al-Dahleh on 2018-06-12.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StockHeader : UICollectionReusableView
+
+@end
---
+++ stockspotter/StockCollectionView/StockHeader/StockHeader.m
@@ -0,0 +1,13 @@
+//
+//  StockHeader.m
+//  StockSpotter
+//
+//  Created by Mohammed Al-Dahleh on 2018-06-12.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+#import "StockHeader.h"
+
+@implementation StockHeader
+
+@end
