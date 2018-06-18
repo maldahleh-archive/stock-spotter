b84895d7394557f14f401c6f86569301849c160c
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-06-18 17:04:14 -0400

    Add gestures to detail view

--- stockspotter/Views/StockDetailView/StockDetailController.m
+++ stockspotter/Views/StockDetailView/StockDetailController.m
@@ -33,6 +33,38 @@
     [self.companyIndustry setText:self.selectedStock.industry];
     [self.companySector setText:self.selectedStock.sector];
     [self.companyURL setText:self.selectedStock.website];
+    
+    [self setupGestures];
+}
+
+#pragma mark - Gestures
+- (void)setupGestures {
+    UITapGestureRecognizer *dismiss = [[UITapGestureRecognizer alloc]
+                                       initWithTarget:self
+                                       action:@selector(dismiss)];
+    dismiss.numberOfTapsRequired = 1;
+    [self.view addGestureRecognizer:dismiss];
+    
+    UISwipeGestureRecognizer *swipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeDismiss)];
+    swipe.direction = UISwipeGestureRecognizerDirectionRight;
+    [self.view addGestureRecognizer:swipe];
+}
+
+- (void)swipeDismiss {
+    [UIView animateWithDuration:0.5 animations:^{
+        self.view.transform = CGAffineTransformMakeTranslation(400, 0);
+    } completion:^(BOOL finished) {
+        [self dismissViewControllerAnimated:true completion:nil];
+    }];
+}
+
+- (void)dismiss {
+    [UIView animateWithDuration:0.75 animations:^{
+        self.view.transform = CGAffineTransformMakeScale(0.01, 0.01);
+        self.view.alpha = 0.0;
+    } completion:^(BOOL finished) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }];
 }
 
 @end
