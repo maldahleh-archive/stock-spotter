73eaf891a8325db626cecb571eca56cc15302df0
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-06-17 21:50:46 -0400

    Create controller for detail view

---
+++ stockspotter/Views/StockDetailView/StockDetailController.h
@@ -0,0 +1,13 @@
+//
+//  StockDetailController.h
+//  StockSpotter
+//
+//  Created by Mohammed Al-Dahleh on 2018-06-17.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StockDetailController : UIViewController
+
+@end
---
+++ stockspotter/Views/StockDetailView/StockDetailController.m
@@ -0,0 +1,21 @@
+//
+//  StockDetailController.m
+//  StockSpotter
+//
+//  Created by Mohammed Al-Dahleh on 2018-06-17.
+//  Copyright © 2018 Mohammed Al-Dahleh. All rights reserved.
+//
+
+#import "StockDetailController.h"
+
+@interface StockDetailController ()
+
+@end
+
+@implementation StockDetailController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
